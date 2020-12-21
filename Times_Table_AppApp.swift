20e9e51dbe02003eeac6dc675c3e580fa245ed5f
asluis <asluisinfo@gmail.com>
2020-12-20 21:17:20 -0800

    Initial upload

---
+++ Times_Table_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Times_Table_AppApp.swift
+//  Times Table App
+//
+//  Created by Luis Alvarez on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Times_Table_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
